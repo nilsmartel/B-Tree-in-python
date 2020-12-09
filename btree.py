51d98ba2b780e566130046911a97f12583e5be4c
Nils Martel <nils.martel@graphmasters.net>
2020-12-09 13:09:51 +0100

    Proper testing

--- btree.py
+++ btree.py
@@ -10,21 +10,6 @@ def split_list(list):
     second = list[index+1:]
     return [first, mid, second]
 
-def test(a, b):
-    if a != b:
-        print(f"[failure] {a} != {b}")
-    else:
-        print("[success]")
-
-def test_split_list():
-    print("test: test_split_list")
-    test([[1], 2, [3]], split_list([1, 2, 3]))
-    test([[1], 2, [3, 4]], split_list([1, 2, 3, 4]))
-    test([[1,2], 3, [4, 5]], split_list([1, 2, 3, 4, 5]))
-
-test_split_list()
-
-
 class BTreeNode:
     def __init__(self, keys = [], parent = None):
         self.parent = None
@@ -153,15 +138,3 @@ class BTree:
             return
 
         self.root.add_key(self, key)
-
-def test_btree():
-    t = BTree()
-    t.add_key('b')
-    for c in "ergha":
-        t.add_key(c)
-    t.add_key('a')
-    for c in "in":
-        t.add_key(c)
-    print(t.string())
-
-test_btree()
